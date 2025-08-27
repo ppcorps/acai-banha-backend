a94ad4cf9e37f85861bba81b49366518354326aa
Pedro Furtado <furtado.pedropaulo@gmail.com>
2025-08-26 22:25:40 -0300

    init commit

---
+++ src/main/java/com/banhaacai/banhaacai/BanhaacaiApplication.java
@@ -0,0 +1,13 @@
+package com.banhaacai.banhaacai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanhaacaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanhaacaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banhaacai/banhaacai/BanhaacaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banhaacai.banhaacai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanhaacaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
